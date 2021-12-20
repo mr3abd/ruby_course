0578b997c12854b0a1d05d35201c64e240ffe5f8
Abdelrhman Haider <abd097@hotmail.com>
2021-12-20 07:48:16 +0200

    Seed Data Students

--- week_1/assessment.rb
+++ week_1/assessment.rb
@@ -1,3 +1,55 @@
 # just return students who's have id when plus next student id will be small than 10
 # make demo with ids from 4 until 15 students  
 # will help you https://apidock.com/ruby/Array/
+
+
+students = [
+    {
+        id: 4,
+        name: "Ahmed"
+    },
+    {
+        id: 5,
+        name: "Yasser"
+    } ,
+    {
+        id: 6,
+        name: "Haider"
+    },
+    {
+        id: 7, 
+        name: "Yazen"
+    },
+    {
+        id: 8, 
+        name: "Ahmed"
+    } ,
+    {
+        id: 9, 
+        name: "Ahmed"
+    },
+    {
+        id: 10, 
+        name: "Ahmed"
+    },
+    {
+        id: 11, 
+        name: "Ahmed"
+    },
+    {
+        id: 12, 
+        name: "Jhon"
+    },
+    {
+        id: 13, 
+        name: "Osama"
+    },
+    {
+        id: 14, 
+        name: "Hassan"
+    },
+    {
+        id: 15, 
+        name: "Mohamed"
+    }
+]
