67dcb4cf55904982b3cc0ecdd74731116d909d75
Abdelrhman Haider <abd097@hotmail.com>
2022-01-22 13:51:15 +0200

    New Quiz for week 4

---
+++ week_4/assessment.rb
@@ -0,0 +1,23 @@
+# Let's Do first App OOP Ruby
+# Start Project with init file can see two classes Student & ClassRoom
+# Student have (name , age , address , phone , class_room_id )
+#   class_room_id: Should Related ClassRoom id and that must be related id from ClassRoom
+# and I can Call by method in @student.get_class_room call ClassRoom
+#
+# ClassRoom (id , name)
+#   id => auto calculate
+#   name => Accept any String
+#
+#
+# Let's Do our story
+#
+#
+# - Will run init file like (ruby init.rb)
+# and Show (Wellcome in School System Please Add how many your Classes Room)
+# when andd like 4 will insert 4 times ClassesRoom and open All Classes Room and say what's count students in that class room
+# and when add 4 stduents make 4 times for insert etc ect until finsh all calsses room
+#
+# and now I can exit from App if make "exit" as string
+# or make search student when write "search"
+# or make insert new classroom when write "insert_new_class_room" with students
+# or make insert new student with choice class_room when write "insert_new_student"
